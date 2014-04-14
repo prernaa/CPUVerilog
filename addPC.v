57637bd84dfb139f6f8dfbcaf10fdb4cfe0bcdce
#PRERNA CHIKERSAL# <PRERNA1@e.ntu.edu.sg>
2014-04-15 05:50:10 +0800

    a NOT working copy of hazard detection

--- addPC.v
+++ addPC.v
@@ -1,7 +1,8 @@
-module addPC(in, out);
+module addPC(in, out, pc_stall);
     input[15:0] in;
+    input pc_stall;
     output[15:0] out;
     
-    assign out = in + 16'b0000000000000001;
+    assign out = (pc_stall!==1'b1)? in + 16'b0000000000000001 : in;
     
 endmodule
--- if_id.v
+++ if_id.v
@@ -1,17 +1,20 @@
 //instruction fetch stage
-module if_id(clk, inst_curr, pc_added, inst_curr_IFID, pc_added_IDIF);
+module if_id(clk, inst_curr, pc_added, inst_curr_IFID, pc_added_IDIF, ifid_stall);
     input[15:0] inst_curr , pc_added;
     input clk;
+    input ifid_stall;
     output [15:0] inst_curr_IFID , pc_added_IDIF;
     reg [15:0] test;
     reg [15:0] pc_added_temp;
     always @ (posedge clk)
-        begin
-        //get instruction 
-        test <= inst_curr;
-        pc_added_temp <= pc_added;
+      begin
+        //get instruction
+        if(ifid_stall!==1'b1) begin 
+          test <= inst_curr;
+          pc_added_temp <= pc_added;
         /*pc_added_IFID <= pc_added;*/
         end
+      end
     assign inst_curr_IFID = test; 
     assign pc_added_IDIF = pc_added_temp;
 
